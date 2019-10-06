80b6cdbd3174bae21d6c23203c41a6c9e4f5b6b0
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-06 13:23:30 +0200

    Add game result - comparison

--- js/script.js
+++ js/script.js
@@ -22,14 +22,51 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if ( playerInput == '1'){
   playerMove = 'kamień';
 }
-else if(playerInput == '2'){
+else if ( playerInput == '2'){
     playerMove = 'papier';
 }
-else if(playerInput == '3'){
+else if ( playerInput == '3'){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+else if ( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage("Remis, zagrajmy jeszcze raz");
+}
+else if ( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Wygrałem !!!, czcesz zagrać jeszcze raz ?');
+}
+else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Wygrałem !!! naostrz nożyce i zagrajmy jeszcze raz');
+    }
+else if ( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Wygrałeś !!! Zagrajmy jeszcze raz, chcę się odegrać');
+    }
+else if ( computerMove =='nożyce' && playerMove == 'nożyce') {
+    printMessage ( 'Nierozegrane, zagrajmy jeszcze !!');
+}
+else if  ( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage ( 'Nierozegrane, szkod że to nie krzemienie, grajmy jeszcze ');
+}
+else if ( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage ('Spróbujmy jeszcze raz, remis');
+}
+else if ( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage ( ' Wygrałeś, potrzebuję czas na naostrzenie nożyc ;) ');
+}
+else if ( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+    printMessage ( ' Wybierz wartość pomiędzy 1 a 3, zagrajmy jeszcze raz ;) ');
+}
+else if ( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+    printMessage ( ' Wprowadzona wartość jest spoza przedziału 1 - 3, zagrajmy jeszcze raz ;) ');
+}
+else if ( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+    printMessage ( ' Możesz wybierać tylko wartości z przedziału 1 - 3, zagrajmy jeszcze raz ');
+}
