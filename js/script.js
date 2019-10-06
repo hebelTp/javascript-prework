e6a926d4369f55fb601cc72dcee9b23506d4b955
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-06 10:44:33 +0200

    Add & use variable playerMove

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 let computerMove ='kamień';
-printMessage('Zagrałem ' + computerMove + ' Jeśli Twój ruch to papier, to wygrywasz!');
+let playerMove ='papier';
+printMessage('Zagrałem ' + computerMove + ' Jeśli Twój ruch to ' + playerMove +' , to wygrywasz!');
+
