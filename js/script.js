0d8311c27fefa75b243eeacce24d718eaa488bf1
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-22 18:33:47 +0200

    Fix indent script.js

--- js/script.js
+++ js/script.js
@@ -82,10 +82,10 @@ function playGame(playerInput){
 }
 document.getElementById('play-rock').addEventListener('click', function(){
     playGame(1);
-  });
+});
 document.getElementById('play-paper').addEventListener('click', function(){
     playGame(2);
-  });
+});
 document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);
-  });  
+});  
