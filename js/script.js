c296f2c752152e56c071503f07f98ed584ebf453
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-22 15:08:31 +0200

    Add button with function

--- js/script.js
+++ js/script.js
@@ -93,3 +93,12 @@ function playGame(playerInput){
 
             displayResult(argComputerMove,argPlayerMove);
 }
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+  });
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+  });
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });  
