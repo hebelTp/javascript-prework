d348a5946f5e92a20ca6bfdfbf4b663892effa31
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-23 12:41:55 +0200

    Remove all coments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-function playGame(playerInput){
+
+
+    function playGame(playerInput){
     clearMessages()
+    
     function getMoveName(argMoveId){
         if(argMoveId == 1){
             return 'kamień';
@@ -14,30 +17,10 @@ function playGame(playerInput){
     let randomNumber = Math.floor(Math.random() * 3 + 1);
         console.log('Wylosowana liczba to: ' + randomNumber);
     let argComputerMove = getMoveName(randomNumber);
-        /*
-        if(randomNumber == 1){
-        computerMove = 'kamień';
-        }
-        else if(randomNumber == 2){
-            computerMove = 'papier';
-        }
-        else if(randomNumber == 3){
-            computerMove = 'nożyce'
-        }*/
-        //printMessage('Mój ruch to: ' + computerMove);
-        //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+        
         console.log('Gracz wpisał: ' + playerInput);
     let argPlayerMove = getMoveName(playerInput);
-        /*
-        if ( playerInput == '1'){
-            playerMove = 'kamień';
-        }
-        else if ( playerInput == '2'){
-            playerMove = 'papier';
-        }
-           else if ( playerInput == '3'){
-            playerMove = 'nożyce';
-        }*/
+        
     function displayResult(argComputerMove, argPlayerMove){
         if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
             printMessage('Ty wygrywasz!');
