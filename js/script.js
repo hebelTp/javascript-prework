87fc2870a413b2994d3263f335d499573b90e62e
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-24 11:35:02 +0200

    Fix  problem indent -  exercise

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 {
-    const playGame = function(playerInput){
+    const playGame = function(playerInput) {
     clearMessages()
     
-    const getMoveName =function (argMoveId){
-        if(argMoveId == 1){
+    const getMoveName =function (argMoveId) {
+        if (argMoveId == 1) {
             return 'kamień';
-        } else if(argMoveId == 2){
+        } else if (argMoveId == 2) {
             return 'papier';
-        } else if(argMoveId == 3){
+        } else if (argMoveId == 3) {
             return 'nożyce';
         }
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
@@ -20,39 +20,39 @@
         console.log('Gracz wpisał: ' + playerInput);
     const argPlayerMove = getMoveName(playerInput);
         
-    const displayResult = function(argComputerMove, argPlayerMove){
-        if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    const displayResult = function(argComputerMove, argPlayerMove) {
+        if ( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
             printMessage('Ty wygrywasz!');
-        } else if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
+        } else if ( argComputerMove == 'papier' && argPlayerMove == 'papier') {
             printMessage("Remis, zagrajmy jeszcze raz");
-        } else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+        } else if ( argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
             printMessage('Wygrałem !!!, czcesz zagrać jeszcze raz ?');
-        } else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+        } else if ( argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
             printMessage('Wygrałem !!! naostrz nożyce i zagrajmy jeszcze raz');
-        } else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+        } else if ( argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
             printMessage('Wygrałeś !!! Zagrajmy jeszcze raz, chcę się odegrać');
-        } else if( argComputerMove =='nożyce' && argPlayerMove == 'nożyce') {
+        } else if ( argComputerMove =='nożyce' && argPlayerMove == 'nożyce') {
             printMessage ( 'Nierozegrane, zagrajmy jeszcze !!');
-        } else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+        } else if ( argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
             printMessage ( 'Nierozegrane, szkod że to nie krzemienie, grajmy jeszcze ');
-        } else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+        } else if ( argComputerMove == 'papier' && argPlayerMove == 'kamień') {
             printMessage ('Spróbujmy jeszcze raz, remis');
-        } else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+        } else if ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
             printMessage ( ' Wygrałeś, potrzebuję czas na naostrzenie nożyc ;) ');
-        } else if( argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch'){
+        } else if ( argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch') {
             printMessage ( ' Wybierz wartość pomiędzy 1 a 3, zagrajmy jeszcze raz ;) ');
-        } else if( argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch'){
+        } else if ( argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch') {
             printMessage ( ' Wprowadzona wartość jest spoza przedziału 1 - 3, zagrajmy jeszcze raz ;) ');
-        } else if( argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch'){
+        } else if ( argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch') {
             printMessage ( ' Możesz wybierać tylko wartości z przedziału 1 - 3, zagrajmy jeszcze raz ');
         } 
     }
     displayResult(argComputerMove,argPlayerMove);
 }
-document.getElementById('play-rock').addEventListener('click', function(){
-    playGame(1);
+document.getElementById('play-rock').addEventListener('click', function() {
+    playGame(1); 
 });
-document.getElementById('play-paper').addEventListener('click', function(){
+document.getElementById('play-paper').addEventListener('click', function() {
     playGame(2);
 });
 document.getElementById('play-scissors').addEventListener('click', function(){
